99df896eb6d427bdc8ff4f66012aeb0eb83468b5
Akash Shukla <akash.shukla@netapp.com>
2018-02-08 03:24:17 +0530

    Start gui form added to meeting item

---
+++ Meeting Room Booking Add In/RoomSelectionGui.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Meeting_Room_Booking_Add_In
+{
+    [System.ComponentModel.ToolboxItemAttribute(false)]
+    partial class RoomSelectionGui : Microsoft.Office.Tools.Outlook.FormRegionBase
+    {
+        public RoomSelectionGui(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            : base(Globals.Factory, formRegion)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RoomSelectionGui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "RoomSelectionGui";
+            this.Size = new System.Drawing.Size(1460, 753);
+            this.FormRegionShowing += new System.EventHandler(this.RoomSelectionGui_FormRegionShowing);
+            this.FormRegionClosed += new System.EventHandler(this.RoomSelectionGui_FormRegionClosed);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #region Form Region Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private static void InitializeManifest(Microsoft.Office.Tools.Outlook.FormRegionManifest manifest, Microsoft.Office.Tools.Outlook.Factory factory)
+        {
+            manifest.FormRegionName = "Select Room";
+            manifest.ShowReadingPane = false;
+
+        }
+
+        #endregion
+
+        public partial class RoomSelectionGuiFactory : Microsoft.Office.Tools.Outlook.IFormRegionFactory
+        {
+            public event Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler FormRegionInitializing;
+
+            private Microsoft.Office.Tools.Outlook.FormRegionManifest _Manifest;
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public RoomSelectionGuiFactory()
+            {
+                this._Manifest = Globals.Factory.CreateFormRegionManifest();
+                RoomSelectionGui.InitializeManifest(this._Manifest, Globals.Factory);
+                this.FormRegionInitializing += new Microsoft.Office.Tools.Outlook.FormRegionInitializingEventHandler(this.RoomSelectionGuiFactory_FormRegionInitializing);
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            public Microsoft.Office.Tools.Outlook.FormRegionManifest Manifest
+            {
+                get
+                {
+                    return this._Manifest;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.IFormRegion Microsoft.Office.Tools.Outlook.IFormRegionFactory.CreateFormRegion(Microsoft.Office.Interop.Outlook.FormRegion formRegion)
+            {
+                RoomSelectionGui form = new RoomSelectionGui(formRegion);
+                form.Factory = this;
+                return form;
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            byte[] Microsoft.Office.Tools.Outlook.IFormRegionFactory.GetFormRegionStorage(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                throw new System.NotSupportedException();
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            bool Microsoft.Office.Tools.Outlook.IFormRegionFactory.IsDisplayedForItem(object outlookItem, Microsoft.Office.Interop.Outlook.OlFormRegionMode formRegionMode, Microsoft.Office.Interop.Outlook.OlFormRegionSize formRegionSize)
+            {
+                if (this.FormRegionInitializing != null)
+                {
+                    Microsoft.Office.Tools.Outlook.FormRegionInitializingEventArgs cancelArgs = Globals.Factory.CreateFormRegionInitializingEventArgs(outlookItem, formRegionMode, formRegionSize, false);
+                    this.FormRegionInitializing(this, cancelArgs);
+                    return !cancelArgs.Cancel;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            [System.Diagnostics.DebuggerNonUserCodeAttribute()]
+            Microsoft.Office.Tools.Outlook.FormRegionKindConstants Microsoft.Office.Tools.Outlook.IFormRegionFactory.Kind
+            {
+                get
+                {
+                    return Microsoft.Office.Tools.Outlook.FormRegionKindConstants.WindowsForms;
+                }
+            }
+        }
+    }
+
+    partial class WindowFormRegionCollection
+    {
+        internal RoomSelectionGui RoomSelectionGui
+        {
+            get
+            {
+                foreach (var item in this)
+                {
+                    if (item.GetType() == typeof(RoomSelectionGui))
+                        return (RoomSelectionGui)item;
+                }
+                return null;
+            }
+        }
+    }
+}
--- Meeting Room Booking Add In/ThisAddIn.cs
+++ Meeting Room Booking Add In/ThisAddIn.cs
@@ -10,14 +10,42 @@ namespace Meeting_Room_Booking_Add_In
 {
     public partial class ThisAddIn
     {
+        //Inspector represents the window in which an Outlook Item is displayed
+        //This ispector field maintains a reference to the collection of Inspector windows in the current Outlook instance
+        //This reference prevents the garbage collector from freeing the memory that contains the event handler for the
+        //E:Microsoft.Office.Interop.Outlook.InspectorsEvents_Event.NewInspector event.
+        Outlook.Inspectors inspectors;
+
+        //Event Handler ThisAddIn_Startup runs as soon as the add-in is clicked
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            //Attach an event handler to the new inspector event
+            inspectors = this.Application.Inspectors;
+            inspectors.NewInspector += Inspectors_NewInspector;
         }
 
+        //Deprecated. ThisAddIn_Shutdown runs as soon as the add-in is closed
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
         {
             // Note: Outlook no longer raises this event. If you have code that 
             //    must run when Outlook shuts down, see https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+
+        //Method gets executed at the New Inspector event
+        private void Inspectors_NewInspector(Outlook.Inspector Inspector)
+        {
+            //add a sample body text onto meeting item
+            Outlook.AppointmentItem appointmentItem = Inspector.CurrentItem;
+            if(appointmentItem != null)
+            {
+                //the EntryID property is not set for an Outlook item until it is saved or sent
+                if (appointmentItem.EntryID==null)
+                {
+                    //append the body of the meeting item
+                    appointmentItem.Body = "Meeting Room Booking Addin";
+                }
+            }
         }
 
         #region VSTO generated code
